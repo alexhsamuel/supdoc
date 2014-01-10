0e71fe167be150e2cf0bc2d075648bc4803ea4c2
Alex Samuel <alex@alexsamuel.net>
2014-01-10 01:24:18 -0500

    Add bootstrap.

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,12 +2,30 @@
 
 var moduleListApp = angular.module('moduleListApp', []);
 
+var types = ["module", "class", "function"];
+
 moduleListApp.controller(
   'ModuleListCtrl',
   function ($scope, $http) {
     $http.get('apidoc.json').success(function (data) {
       $scope.modules = data;
       $scope.module = "";
+
+      $scope.sortDict = function (obj) {
+        var result = [];
+        for (name in obj) 
+          result.push([name, obj[name]]);
+        result.sort(function (item0, item1) { 
+          var t0 = types.indexOf(item0[1].type);
+          var t1 = types.indexOf(item1[1].type);
+          var n0 = item0[1].name;
+          var n1 = item1[1].name;
+          return t0 < t1 ? -1 : t0 > t1 ?  1 : n0 < n1 ? -1 : n0 > n1 ?  1 : 0;
+        });
+        return result;
+      };
     });
 });
+
+//------------------------------------------------------------------------------
 
