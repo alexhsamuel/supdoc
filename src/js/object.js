f4363bb11d45412d1c60b44b8c0a59c91f3b2a9a
Alex Samuel <alex@alexsamuel.net>
2014-02-16 16:47:23 -0500

    WIP

--- src/js/object.js
+++ src/js/object.js
@@ -16,21 +16,36 @@ ObjectModule.controller(
       'api',
       function (api) {
         $scope.submoduleNames = 
-          api && api.modules 
-          ? mapObjToArr(api.modules, function (_, m) { return m.fqname })
-          : []
+        api && api.modules 
+        ? mapObjToArr(api.modules, function (_, m) { return m.fqname })
+        : []
       })
 
     $scope.getByType = function (type) {
-        var result = {}
-        var dict = $scope.api ? $scope.api.dict : []
-        for (var name in dict) {
-            var obj = dict[name]
-            if (obj.type == type)
-                result[name] = obj
-        }
-        return result
+      var result = {}
+      var dict = $scope.api ? $scope.api.dict : []
+      for (var name in dict) {
+        var obj = dict[name]
+        if (obj.type == type)
+          result[name] = obj
+      }
+      return result
     }
+
+    $scope.formatParameters = function (parameters) {
+      var result = "("
+        var first = true
+        for (var i = 0; i < parameters.length; ++i) {
+          var param = parameters[i]
+          if (first) 
+            first = false
+          else
+            result += ", "
+          result += param.name
+        }
+        result += ")"
+        return result
+      }
 
   })
 
