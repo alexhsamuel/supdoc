aa2c641d8243eb8fc87c10fe2a33c3522b23f96f
Alex Samuel <alex@alexsamuel.net>
2014-03-30 12:33:45 -0400

    Local env.sh.

--- env.sh
+++ env.sh
@@ -1,6 +1,9 @@
-export PATH=$HOME/sw/Python-3.4.0b1/bin:$PATH
+PYTHON_HOME=$HOME/sw/Python-3.4.0b1
+export PATH=$PYTHON_HOME/bin:$PATH
 
 BASE=$HOME/dev/apidoc
 export PYTHONPATH=$BASE/src/python
 export PATH=$BASE/bin:$PATH
 
+# alias py3=$PYTHON_HOME/bin/python3
+function py3() { /Library/Frameworks/Python.framework/Versions/3.4/bin/python3 -q "$@"; }
