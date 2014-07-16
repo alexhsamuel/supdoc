fad704f1f6843a8ba5d673fd7eca4cf2b46d5acc
Alex Samuel <alex@alexsamuel.net>
2014-07-15 22:17:05 -0400

    Split out environment config.

--- env.sh
+++ env.sh
@@ -1,9 +1,4 @@
-PYTHON_HOME=$HOME/sw/Python-3.4.0b1
+. $(dirname $BASH_ARGV)/cfg.sh
 export PATH=$PYTHON_HOME/bin:$PATH
-
-BASE=$HOME/dev/apidoc
 export PYTHONPATH=$BASE/src/python
-export PATH=$BASE/bin:$PATH
-
-# alias py3=$PYTHON_HOME/bin/python3
-function py3() { /Library/Frameworks/Python.framework/Versions/3.4/bin/python3 -q "$@"; }
+function py3() { $PYTHON_HOME/bin/python3 -q "$@"; }
