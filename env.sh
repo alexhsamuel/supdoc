df5d2d688131038921829a83ae26becb996daa62
Alex Samuel <alex@alexsamuel.net>
2015-12-18 08:01:19 +0800

    Use Plynth.

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 . $(dirname $BASH_ARGV)/cfg.sh
 export PATH=$PYTHON_HOME/bin:$PATH
-export PYTHONPATH=$BASE/src/python
+export PYTHONPATH=$PLYNTH:$BASE/src/python
 function py3() { $PYTHON_HOME/bin/python3 -q "$@"; }
