d39ae3874bd83e323fbf11268abb25203cc8536b
Alex Samuel <alex@alexsamuel.net>
2016-01-10 23:05:35 -0500

    Include test inputs in PYTHONPATH.

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 . $(dirname $BASH_ARGV)/cfg.sh
 export PATH=$PYTHON_HOME/bin:$PATH
-export PYTHONPATH=$PLYNTH:$BASE/src/python
+export PYTHONPATH=$PLYNTH:$BASE/src/python:$BASE/test/input/src
 function py3() { $PYTHON_HOME/bin/python3 -q "$@"; }
 function supdoc() { clear; py3 -m supdoc.text "$@" | less -eF; }
