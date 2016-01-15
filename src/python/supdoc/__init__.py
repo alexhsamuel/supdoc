83021cb3abe5d3a7abb3305b896709e118033d8e
Alex Samuel <alex@alexsamuel.net>
2016-01-14 22:18:35 -0500

    Improve supdoc().

--- src/python/supdoc/__init__.py
+++ src/python/supdoc/__init__.py
@@ -1,17 +1,24 @@
+import json as _json
 import sys
 
 from   . import inspector
 from   . import text
+import pln.terminal
+import pln.terminal.printer
 
 #-------------------------------------------------------------------------------
 
-def supdoc(obj):
+def supdoc(obj, *, json=False):
     docsrc = inspector.DocSource()
     objdoc = docsrc._DocSource__inspector._inspect(obj)
-    import json
-    json.dump(objdoc, sys.stderr, indent=2)
-    print(file=sys.stderr)
-    text.print_docs(docsrc, objdoc)
+
+    if json:
+        _json.dump(objdoc, sys.stdout, indent=2)
+        print()
+    else:
+        width = pln.terminal.get_width() - 1 
+        printer = pln.terminal.printer.Printer(indent=" ", width=width)
+        text.print_docs(docsrc, objdoc, printer=printer)
 
 
 
