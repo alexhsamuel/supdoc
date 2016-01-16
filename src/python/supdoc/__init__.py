4b1b0a53af1aea23790c28637597249926da68ee
Alex Samuel <alex@alexsamuel.net>
2016-01-15 21:06:47 -0500

    Rename text.py to terminal.py.  Improve supdoc().

--- src/python/supdoc/__init__.py
+++ src/python/supdoc/__init__.py
@@ -2,14 +2,30 @@ import json as _json
 import sys
 
 from   . import inspector
-from   . import text
+from   . import terminal
+from   pln.memo import memoize
 import pln.terminal
-import pln.terminal.printer
+from   pln.terminal.printer import Printer
+
+__all__ = (
+    "supdoc", 
+    )
 
 #-------------------------------------------------------------------------------
 
+@memoize
+def _get_docsrc():
+    return inspector.DocSource()
+
+
 def supdoc(obj, *, json=False):
-    docsrc = inspector.DocSource()
+    """
+    Prints documentation for `obj`.
+
+    Inspects `obj` for documentation and formats it for the terminal.  If 
+    `json` is true, dumps the JSON representation of the documentation instead.
+    """
+    docsrc = _get_docsrc()
     objdoc = docsrc._DocSource__inspector._inspect(obj)
 
     if json:
@@ -17,8 +33,8 @@ def supdoc(obj, *, json=False):
         print()
     else:
         width = pln.terminal.get_width() - 1 
-        printer = pln.terminal.printer.Printer(indent=" ", width=width)
-        text.print_docs(docsrc, objdoc, printer=printer)
+        printer = Printer(indent=" ", width=width)
+        terminal.print_docs(docsrc, objdoc, printer=printer)
 
 
 
